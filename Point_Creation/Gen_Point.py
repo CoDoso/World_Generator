531704eff06bbf9b11c529d4d70444a9acdaf871
CoDoso <155891549+CoDoso@users.noreply.github.com>
2024-06-06 08:34:16 +0200

    Wonders Update for  Point Generation

--- Point_Creation/Gen_Point.py
+++ Point_Creation/Gen_Point.py
@@ -4,13 +4,10 @@ import Gen_Values as Game
 
 
 def create_points():
-
-    i = 0
     x = 0
     y = 1
 
-    while i < Game.worldsize:
-
+    for i in range(Game.worldsize):
         # Location & ID Calculation
         if True:
             i += 1
@@ -19,8 +16,6 @@ def create_points():
             else:
                 x = 1
                 y += 1
-
-        # print(f"X: {x} | Y: {y}")
 
         # Biome Assigning
         if True:
@@ -32,10 +27,10 @@ def create_points():
             New_Biome = ""
 
             # Elevation
-            Flat_Chance = -10 * 2
-            Hills_Chance = 30 * 2
-            Steep_Chance = 25 * 2  # Base Chances
-            Water_Chance = 40 * 2
+            Flat_Chance = 17.5 * 2
+            Hills_Chance = 22.5 * 2
+            Steep_Chance = 20 * 2  # Base Chances
+            Water_Chance = 25 * 2
             Uninhabitable_Chance = 15 * 2
             New_Elevation = ""
             Water_Status = "NoWater"
@@ -77,7 +72,7 @@ def create_points():
                             Hills_Chance += 12.5
                             Steep_Chance += 7.5
                             Water_Chance -= 20
-                            Uninhabitable_Chance -= 5    # Chance Calculations for Terrain
+                            Uninhabitable_Chance -= 5  # Chance Calculations for Terrain
                         case "Steep":
                             Flat_Chance -= 30
                             Hills_Chance += 20
@@ -146,8 +141,6 @@ def create_points():
                             Forest_Chance -= 10
                             Water_Chance -= 5
 
-            BaE_Chance = random.randint(1, 200)  # BaE = Biome and Elevation
-
             if Grassland_Chance < 0:
                 Grassland_Chance = 0
             if Forest_Chance < 0:
@@ -155,7 +148,7 @@ def create_points():
             if Sand_Chance < 0:
                 Sand_Chance = 0
 
-            if Flat_Chance < 0:    # This insures there's no negative Chance
+            if Flat_Chance < 0:  # This insures there's no negative Chance
                 Flat_Chance = 0
             if Hills_Chance < 0:
                 Hills_Chance = 0
@@ -166,35 +159,45 @@ def create_points():
             if Uninhabitable_Chance < 0:
                 Uninhabitable_Chance = 0
 
-            if (BaE_Chance / 2) < Grassland_Chance:
+            Biome_Chance = random.randint(1, Sand_Chance + Forest_Chance + Grassland_Chance)
+            Elevation_Chance = random.randint(1,
+                                              int(Uninhabitable_Chance + Water_Chance + Steep_Chance + Hills_Chance + Flat_Chance))
+
+            if (Biome_Chance / 2) < Grassland_Chance:
                 New_Biome = Game.Biomes[0]
-            elif (BaE_Chance / 2) < Forest_Chance + Grassland_Chance:
+            elif (Biome_Chance / 2) < Forest_Chance + Grassland_Chance:
                 New_Biome = Game.Biomes[1]
-            elif (BaE_Chance / 2) < Sand_Chance + Forest_Chance + Grassland_Chance:
+            elif (Biome_Chance / 2) < Sand_Chance + Forest_Chance + Grassland_Chance:
                 New_Biome = Game.Biomes[2]
 
-            if BaE_Chance < Flat_Chance:                 # This assigns the Biome and Elevation
+            if Elevation_Chance < Flat_Chance:  # This assigns the Biome and Elevation
                 New_Elevation = Game.Elevations[0]
-            elif BaE_Chance < Hills_Chance + Flat_Chance:  # ToDo: Wonders
+            elif Elevation_Chance < Hills_Chance + Flat_Chance:  # ToDo: Wonders
                 New_Elevation = Game.Elevations[1]
-            elif BaE_Chance < Steep_Chance + Hills_Chance + Flat_Chance:
+                if random.randint(0, 20) == 10:
+                    for Wonder in Game.Remaining_Wonders:
+                        if Wonder == {"High Hills": {"Type": "Military", "Modifier": "ToDo [Local] Fort Level +1"}}:
+                            Wonders.append(Wonder)
+                            Game.Remaining_Wonders.remove(Wonder)  # Great Falls Monument(Wonder) creation
+                            print(f"High Hills ID: {i}")
+            elif Elevation_Chance < Steep_Chance + Hills_Chance + Flat_Chance:
                 New_Elevation = Game.Elevations[2]
-            elif BaE_Chance < Water_Chance + Steep_Chance + Hills_Chance + Flat_Chance:
+            elif Elevation_Chance < Water_Chance + Steep_Chance + Hills_Chance + Flat_Chance:
                 New_Elevation = Game.Elevations[3]
                 if Water_Status != "River":
                     Water_Status = "Source"
-            elif BaE_Chance < Uninhabitable_Chance + Water_Chance + Steep_Chance + Hills_Chance + Flat_Chance:
+            elif Elevation_Chance < Uninhabitable_Chance + Water_Chance + Steep_Chance + Hills_Chance + Flat_Chance:
                 New_Elevation = Game.Elevations[4]
             else:
                 New_Elevation = Game.Elevations[random.randint(0, 4)]
                 New_Biome = Game.Biomes[random.randint(0, 2)]
 
-            if (New_Elevation != "Water" or New_Elevation == "Steep") and Water_Status == "River":
+            if (New_Elevation == "Steep" or New_Elevation != "Water") and Water_Status == "River":
                 if New_Elevation == "Steep":
                     Water_Status = "Waterfall"
-                    if random.randint(0, int(Game.worldsize*0.085)) == 5:
+                    if random.randint(0, 20) == 10:
                         for Wonder in Game.Remaining_Wonders:
-                            if Wonder == {'Great Falls': {'Type': 'Touristic', 'Modifier': 'ToDo [Global] Prestige +10'}}:
+                            if Wonder == {"Great Falls": {"Type": "Symbolic", "Modifier": "ToDo [Faction] Prestige +10"}}:
                                 Wonders.append(Wonder)
                                 Game.Remaining_Wonders.remove(Wonder)  # Great Falls Monument(Wonder) creation
                                 print(f"Great Falls ID: {i}")
@@ -268,9 +271,9 @@ def create_points():
             Forestry_Land = 0
             Infrastructure = 0
             Wild_Hunt = 0
-            Berrys = 0                    # Base Values
-            Mushrooms = 0                 # This is for convenience
-            Mana_Crystals = 0             # (If you want a game were every province has Gold this makes it possible)
+            Berrys = 0  # Base Values
+            Mushrooms = 0  # This is for convenience
+            Mana_Crystals = 0  # (If you want a game were every province has Gold this makes it possible)
             Gems = 0
             Gold = 0
 
@@ -301,7 +304,14 @@ def create_points():
                     Mushrooms += random.randint(2, 10)
                 case "Sand":
                     Score += 1
-
+                    if New_Elevation == "Water":
+                        if random.randint(0, 20) == 10:
+                            for Wonder in Game.Remaining_Wonders:
+                                if Wonder == {"Desert Floodplains": {"Type": "Economic",
+                                                                     "Modifier": "ToDo [Local] +15 Fertile Lands"}}:
+                                    Wonders.append(Wonder)
+                                    Game.Remaining_Wonders.remove(Wonder)  # Desert Floodplains creation
+                                    print(f"Desert Floodplains ID: {i}")
             match New_Elevation:
                 case "Flat":
                     Score += 5
@@ -352,6 +362,16 @@ def create_points():
             # blood and despair.
             # Unlike Mountains or Oceans these are however passable
             # They are also very easy to defend (through Defender Bonuses unique to this terrain)
+
+            if random.randint(0, 25) == 10:
+                for Wonder in Game.Remaining_Wonders:
+                    if Wonder == {"Volcano Field": {"Type": "Symbolic",
+                                                    "Modifier": "ToDo [Both] Allow Volcano Eruption Event & *2 Attrition"}}:
+                        Wonders.append(Wonder)
+                        Score = int(Score / 2)
+                        Game.Remaining_Wonders.remove(Wonder)  # Volcano Field Wonder creation
+                        print(f"Volcano Field ID: {i}")
+
             New_Point = {
                 "Point": {
                     "Point_ID": i,
@@ -362,13 +382,13 @@ def create_points():
                     "Terrain": {
                         "Biome": New_Biome,
                         "Elevation": New_Elevation,
-                        "Army Limit": Score*20  # Grassland = 60, Forest = 40, Sand = 20
+                        "Army Limit": Score * 20  # Grassland = 60, Forest = 40, Sand = 20
                         # If the Army on this Point is larger than the Limit the Army will start taking Damage
                         # every single turn called "Attrition" inspiration EU4
                     },
-                    "Monuments": {  # The difference between Wonders and Buildings is Wonders
-                        "Wonders": Wonders,  # are based on Terrain and get destroyed if those change
-                    }
+                    # The difference between Wonders and Buildings is Wonders
+                    # are based on Terrain and get destroyed if those change
+                    "Wonders": Wonders,
                 }
             }
         else:
@@ -386,15 +406,70 @@ def create_points():
                 match rs:  # This is to check if the Resource exists in that Point
                     case "Iron":  # and if not don't put it into the Data
                         if Iron != 0:
+                            if random.randint(0, int(Game.worldsize * 0.085)) == 5:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Great Iron Deposit": {"Type": "Economic",
+                                                                         "Modifier": "ToDo [Local] +30 Iron"}}:
+                                        Wonders.append(Wonder)
+                                        Iron += 30
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        print(f"Great Iron Deposit ID: {i}")
                             Resources.append({"Iron": Iron})
                     case "Coal":
                         if Coal != 0:
+                            if random.randint(0, int(Game.worldsize * 0.085)) == 5:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Great Coal Deposit": {"Type": "Economic",
+                                                                         "Modifier": "ToDo [Local] +30 Coal"}}:
+                                        Wonders.append(Wonder)
+                                        Coal += 30
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        print(f"Great Coal Deposit ID: {i}")
+                            if New_Biome == "Sand" and random.randint(0, int(Game.worldsize * 0.085)) == 5:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Senlows Coal Deposit": {"Type": "Economic",
+                                                                           "Modifier": "ToDo [Local] +50 Coal"}}:
+                                        Wonders.append(Wonder)
+                                        Coal += 30
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        print(f"Senlows Coal Deposit ID: {i}")
                             Resources.append({"Coal": Coal})
                     case "Copper":
                         if Copper != 0:
-                            Resources.append({"Copper": Copper})
+                            if random.randint(0, int(Game.worldsize * 0.085)) == 5:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Great Copper Deposit": {"Type": "Economic",
+                                                                           "Modifier": "ToDo [Local] +30 Copper"}}:
+                                        Wonders.append(Wonder)
+                                        Copper += 30
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        print(f"Great Copper Deposit ID: {i}")
+                                        Resources.append({"Copper": Copper})
+                            elif random.randint(0, 20) == 5 and Tin != 0:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Bronze Mountain": {"Type": "Economic",
+                                                                      "Modifier": "ToDo [Local] +40 Copper&Tin"}}:
+                                        Wonders.append(Wonder)
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        Copper += 40
+                                        Tin += 40
+                                        print(f"Bronze Mountain ID: {i}")
+                                        Resources.append({"Copper": Copper})
+                            else:
+                                Resources.append({"Copper": Copper})
                     case "Tin":
                         if Tin != 0:
+                            if random.randint(0, int(Game.worldsize * 0.085)) == 5:
+                                for Wonder in Game.Remaining_Wonders:
+                                    if Wonder == {"Bronze Mountain": {"Type": "Economic",
+                                                                      "Modifier": "ToDo [Local] +40 Copper&Tin"}}:
+                                        break
+                                    if Wonder == {
+                                        "Great Tin Deposit": {"Type": "Economic", "Modifier": "ToDo [Local] +30 Tin"}}:
+                                        Wonders.append(Wonder)
+                                        Tin += 30
+                                        Game.Remaining_Wonders.remove(Wonder)
+                                        print(f"Great Tin Deposit ID: {i}")
                             Resources.append({"Tin": Tin})
                     case "Wild Hunt":
                         if Wild_Hunt != 0:
@@ -428,17 +503,12 @@ def create_points():
                         "Water_Status": Water_Status
                     },
                     "Resources": Resources,
-                    "Monuments": {  # The difference between Wonders and Buildings is Wonders
-                        "Wonders": Wonders,  # are based on Terrain and get destroyed if those change
-                        "Buildings": {}  # Buildings do not get destroyed if the Terrain get changed
-                    }
+                    "Wonders": Wonders,  # are based on Terrain and get destroyed if those change
                 }
             }
-
-        if New_Elevation == "Uninhabitable":
-            print(New_Biome)
 
         Game.points.append(New_Point)
 
     with open("points.yml", 'w') as f:
         yaml.safe_dump(Game.points, f, default_flow_style=False, sort_keys=False)
+        
