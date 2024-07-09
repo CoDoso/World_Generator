06eb53af9b4b4af0eb97a6db3260d4abc215330c
CoDoso <155891549+CoDoso@users.noreply.github.com>
2024-07-09 16:19:48 +0200

    small update

--- Point_Creation/Gen_Values.py
+++ Point_Creation/Gen_Values.py
@@ -1,7 +1,12 @@
+# -------------------------------------------------------------------------------------------------------------------
+#                                            '---[ Points ]---'
+#
+# Worldsize
 worldsize_input_x = int(input("Insert X size for the World size: "))
-worldsize_input_y = int(input("\nInsert Y size for the World size: "))
+worldsize_input_y = int(input("Insert Y size for the World size: "))
 worldsize = worldsize_input_x * worldsize_input_y
-
+#
+# Point Data
 rows_left = worldsize_input_y
 points = []
 Biomes = ["Grassland", "Forest", "Sand"]
@@ -22,3 +27,54 @@ Natural_Wonders = [
     {"Great Tin Deposit": {"Type": "Economic", "Modifier": "ToDo [Local] +30 Tin"}}]  # 9
 # Modifier: Faction Variable(Global Modifiers or "Local"(the Point)) Increase | (Modifiers Inspiration: EU4)
 Remaining_Wonders = Natural_Wonders
+#
+# -------------------------------------------------------------------------------------------------------------------
+#                                           '---[ Factions ]---'
+# Faction Diversity
+faction_amount = int(input("Amount of Factions at Gamestart: "))
+#
+# Races
+Races = ["Human", "Elf", "Dwarf", "Orc", "Fallen"]
+#
+# Faction Data
+factions = []
+#
+# -------------------------------------------------------------------------------------------------------------------
+#                                           '---[ Modifiers ]---'
+# Economy
+City_Creation_cost = 350  # Cost to build a City in a Point
+Tax_Multiplier = 1
+Production_Efficiency = 1
+Tariff_Multiplier = 1
+# Technology
+Research_Speed = 100
+Research_Queues = 1  # Like in Call of Dragons
+# Military
+Army_Efficiency = 100  # Eu4 Morale
+Army_Damage_Dealt = 100  # Eu4 Discipline
+
+Levy_Multiplier = 1  # similar to Eu4 "Manpower" Modifiers
+Levy_Damage = 25
+Levy_Defense = 30  # Levy's   (Stats similar to CK3)
+Levy_Pursuit = 0
+
+Knight_Damage = 60
+Knight_Defense = 110  # Knight
+Knight_Pursuit = 0
+
+Archer_Damage = 75
+Archer_Defense = 20  # Archer
+Archer_Pursuit = 5
+
+Mage_Damage = 80
+Mage_Defense = 15    # Mage
+Mage_Pursuit = 5
+
+Cavalry_Damage = 60
+Cavalry_Defense = 75  # Cav
+Cavalry_Pursuit = 25
+
+# Diplomatic
+Reputation = 50
+Prestige = 0  # Affect likeness to Ally, Relations, etc
+Max_Allies = 3
